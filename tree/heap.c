55e07444a0343afbf7f3a13fc2ec50bf42e1b35c
yanqi <3224848830@qq.com>
2024-10-27 01:21:59 +0800

    feat: 大顶堆 小顶堆

--- tree/heap.c
+++ tree/heap.c
@@ -14,7 +14,7 @@ typedef struct MaxHeap {
 
 _Bool initHeap(Heap heap){   //初始化都是老套路了，不多说了
     heap->size = 0;
-    heap->capacity = 10;
+    heap->capacity = 11;
     heap->arr = malloc(sizeof (E) * heap->capacity);
     return heap->arr != NULL;
 }
@@ -48,21 +48,54 @@ E delete(Heap heap){
     return max;
 }
 
+_Bool init_MinHeap(Heap heap){
+    heap->size = 0;
+    heap->capacity = 10;
+    heap->arr = malloc(sizeof (E) * heap->capacity);
+    return heap->arr != NULL;
+}
+
+_Bool minHeap_insert(Heap heap, E element){
+    if(heap->size == heap->capacity) return 0;
+    int index = ++heap->size;
+    while (index > 1 && element < heap->arr[index / 2]) {
+        heap->arr[index] = heap->arr[index / 2];
+        index /= 2;
+    }
+    heap->arr[index] = element;
+    return 1;
+}
+
+E minHeap_delete(Heap heap){
+    E max = heap->arr[1], e = heap->arr[heap->size--];
+    int index = 1;
+    while (index * 2 <= heap->size) {
+        int child = index * 2;
+        if(child < heap->size && heap->arr[child] > heap->arr[child + 1])
+            child += 1;
+        if(e <= heap->arr[child]) break;
+        else heap->arr[index] = heap->arr[child];
+        index = child;
+    }
+    heap->arr[index] = e;
+    return max;
+}
+
 void printHeap(Heap heap){
     for (int i = 1; i <= heap->size; ++i)
         printf("%d ", heap->arr[i]);
 }
 
-int main(){
-    struct MaxHeap heap;
-    initHeap(&heap);
-    insert(&heap, 5);
-    insert(&heap, 2);
-    insert(&heap, 3);
-    insert(&heap, 7);
-    insert(&heap, 6);
-
-    printf("%d\n", delete(&heap));
-
-    printHeap(&heap);
-}
+// int main(){
+//     struct MaxHeap heap;
+//     initHeap(&heap);
+//     insert(&heap, 5);
+//     insert(&heap, 2);
+//     insert(&heap, 3);
+//     insert(&heap, 7);
+//     insert(&heap, 6);
+//
+//     printf("%d\n", delete(&heap));
+//
+//     printHeap(&heap);
+// }
